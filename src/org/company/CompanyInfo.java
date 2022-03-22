bf387083c76e39a7046e91557c9114ceaa8b5a59
Satheesh1507 <smartstreetsatheesh@gmail.com>
2022-03-22 23:08:50 +0530

    compdet

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,28 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName(String name) {
+		System.out.println("The Employee Name is " + name);
+
+	}
+
+	private void companyName(int id, String name) {
+		System.out.println("The Company Id is " + id + "\n" + "My name is " + name);
+
+	}
+
+	private void companyName(long phoneno, float salary, double avgsalary) {
+		System.out.println("The Employee Phoneno is " + phoneno + "\n" + "The employee salary is " + salary + "\n"
+				+ "The Employee Avg salary is " + avgsalary);
+
+	}
+
+	public static void main(String[] args) {
+		CompanyInfo c = new CompanyInfo();
+		c.companyName("Raj Kumar");
+		c.companyName(8072930743l, 2548.123654f, 225324.02545154);
+		c.companyName(12345, "Alpha");
+		
+	}
+
+}
